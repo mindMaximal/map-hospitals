910fb60b227dff79cf2c0b0842b7b53eccf3c042
Евгений Волков <vamper.2012@gmail.com>
2021-05-15 13:10:49 +0800

    hosting settings

--- App.js
+++ App.js
@@ -17,6 +17,29 @@ app.use('/api/reports', require('./routes/reports.routes'))
 app.use('/api/reports/area', require('./routes/area.routes'))
 app.use('/api/reports/pdf', require('./routes/pdf.routes'))
 
+if (process.env.NODE_ENV === 'production') {
+  app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
+
 const PORT = config.get('port') || 5000
 
 app.listen(PORT, () => console.log(`Server has been started on port ${PORT} ...`))
+
+/*
+alias node='/home/c/cm74536/nodejs/bin/node'
+
+alias npm='/home/c/cm74536/nodejs/bin/npm'
+
+export PATH=$PATH:/home/c/cm74536/nodejs/bin/
+
+
+alias git='/home/c/cm74536/nodejs/bin/node'
+
+export PATH=$PATH:/home/c/cm74536/nodejs/bin/
+
+
+ */
